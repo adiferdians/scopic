93bdc5b9734f04d7c0b2d0405e9e4a4772de4f76
overVoid <adiferdian@outlook.com>
2024-07-24 20:07:22 +0700

    bersih" kode & asset Fe

---
+++ resources/views/view-user/landing-page.blade.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Anyar Bootstrap Template - Index</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets-user/img/favicon.png" rel="icon">
+  <link href="assets-user/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets-user/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets-user/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets-user/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets-user/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets-user/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets-user/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets-user/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets-user/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets-user/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo mr-auto"><a href="#header" class="scrollto">kwepoo</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="#header" class="logo mr-auto scrollto"><img src="assets-user/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="#header">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#team">Team</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li><a href="#contact">Contact</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex justify-cntent-center align-items-center">
+    <div id="heroCarousel" class="container carousel carousel-fade" data-ride="carousel">
+
+      <!-- Slide 1 -->
+      <div class="carousel-item active">
+        <div class="carousel-container">
+          <h2 class="animate__animated animate__fadeInDown">Optimize your Business with <span>Kwepoo</span></h2>
+          <p class="animate__animated animate__fadeInUp">Create more profit and less cost with Efficient and Optimized Business Process</p>
+          <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
+        </div>
+      </div>
+
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>About Us</h2>
+        </div>
+        <div class="row content">
+          <div class="col-lg-6">
+          <p>Kwepoo is a world class software house company in Indonesia. We help our partner and client to optimize business process by implementing technology.
+            Either you are prospectful company/institution or talented people who want to join our team, le'ts have a chat. We will contact you soon and you can track this conversation progress here.</p>
+            <p>
+            We are partner for many optimized companies and institutions. Make sure yours is one of them.
+            </p>
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0">
+            <ul>
+              <li><i class="ri-check-double-line"></i> Optimize Business Process</li>
+              <li><i class="ri-check-double-line"></i> Manage and Maintain your IT/Application/Startup</li>
+              <li><i class="ri-check-double-line"></i> Deliver Disruption & Innovation</li>
+              <li><i class="ri-check-double-line"></i> Develop Web and Mobile App</li>
+              <li><i class="ri-check-double-line"></i> Build, Train and Develop IT Team</li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Us Section -->
+
+    <!-- ======= Clients Section ======= -->
+    <section id="clients" class="clients">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="owl-carousel clients-carousel">
+          <img src="assets-user/img/clients/client-1.png" alt="">
+          <img src="assets-user/img/clients/client-2.png" alt="">
+          <img src="assets-user/img/clients/client-3.png" alt="">
+          <img src="assets-user/img/clients/client-4.png" alt="">
+          <img src="assets-user/img/clients/client-5.png" alt="">
+          <img src="assets-user/img/clients/client-6.png" alt="">
+          <img src="assets-user/img/clients/client-7.png" alt="">
+          <img src="assets-user/img/clients/client-8.png" alt="">
+        </div>
+
+      </div>
+    </section><!-- End Clients Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Services</h2>
+          <p>We Can Help You Develop Web and Mobile Apps</p>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4 d-flex align-items-stretch  mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon-box">
+              <i class="icofont-computer"></i>
+              <h4><a href="#">Describe The Problem</a></h4>
+            </div>
+          </div>
+          <div class="col-md-4 d-flex align-items-stretch mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon-box">
+              <i class="icofont-chart-bar-graph"></i>
+              <h4><a href="#">Define The Objectives</a></h4>
+            </div>
+          </div>
+          <div class="col-md-4 d-flex align-items-stretch mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="300">
+            <div class="icon-box">
+              <i class="icofont-image"></i>
+              <h4><a href="#">Identify Technical Requirements</a></h4>
+            </div>
+          </div>
+          <div class="col-md-4 d-flex align-items-stretch mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="400">
+            <div class="icon-box">
+              <i class="icofont-settings"></i>
+              <h4><a href="#">Design</a></h4>
+            </div>
+          </div>
+          <div class="col-md-4 d-flex align-items-stretch mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="500">
+            <div class="icon-box">
+              <i class="icofont-earth"></i>
+              <h4><a href="#">Develop</a></h4>
+            </div>
+          </div>
+          <div class="col-md-4 d-flex align-items-stretch mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="600">
+            <div class="icon-box">
+              <i class="icofont-tasks-alt"></i>
+              <h4><a href="#">Deploy</a></h4>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Cta Section ======= -->
+    <section id="cta" class="cta">
+      <div class="container">
+
+        <div class="row" data-aos="zoom-in">
+          <div class="col-lg-9 text-center text-lg-left">
+            <h3>Call To Action</h3>
+            <p> Small and medium-sized enterprises (SME) managed services are one of the product developments from Javan, aimed at providing value-added services for clients' 
+              businesses and offering solutions to clients' problems. With this SME, we can build an IT-based system 
+              foundation so that companies can work efficiently, anytime, anywhere, making them ready to scale up. The IT-based managed services we offer are:</p>
+          </div>
+          <div class="col-lg-3 cta-btn-container text-center">
+            <a class="cta-btn align-middle" href="#">Call To Action</a>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Cta Section -->
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Kwepoo</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by overVoid
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets-user/vendor/jquery/jquery.min.js"></script>
+  <script src="assets-user/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets-user/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets-user/vendor/php-email-form/validate.js"></script>
+  <script src="assets-user/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets-user/vendor/venobox/venobox.min.js"></script>
+  <script src="assets-user/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets-user/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets-user/js/main.js"></script>
+
+</body>
+
+</html>
